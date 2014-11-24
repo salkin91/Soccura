8690caf6095a26285cf257a8ec9785a6c5b90d6a
Oliver <Oliver@129.16.79.83>
2014-11-24 12:18:59 +0100

    Everything

--- Match.java
+++ Match.java
@@ -1,1 +1,72 @@
-//här är den här classen
+
+public class Match {
+	private String Hteam;	// This should be the same as a name from an existing Team,
+	private String Ateam;
+	private String Date;	// You can not have a match without 2 existing teams.
+	private int Hscore;		// Int for a score
+	private int Ascore;	
+	private int MID;		// Match ID
+	/**
+	 * Need 2 names for the teams that compete
+	 * First score is for the first team
+	 * Second score is for the second team
+	 * The MID should only ever be specified by the DB
+	 */
+	Match(String ht, String at, String d, int hs, int as, int id){
+		this.setHteam(ht);
+		this.setAteam(at);
+		this.setDate(d);
+		this.setHscore(hs);
+		this.setAscore(as);
+		this.setMID(id);
+	}
+
+	public String getHteam() {
+		return Hteam;
+	}
+
+	public void setHteam(String hteam) {
+		Hteam = hteam;
+	}
+
+	public String getAteam() {
+		return Ateam;
+	}
+
+	public void setAteam(String ateam) {
+		Ateam = ateam;
+	}
+
+	public int getHscore() {
+		return Hscore;
+	}
+
+	public void setHscore(int hscore) {
+		Hscore = hscore;
+	}
+
+	public int getAscore() {
+		return Ascore;
+	}
+
+	public void setAscore(int ascore) {
+		Ascore = ascore;
+	}
+
+	public int getMID() {
+		return MID;
+	}
+
+	public void setMID(int mID) {
+		MID = mID;
+	}
+
+	public String getDate() {
+		return Date;
+	}
+
+	public void setDate(String date) {
+		Date = date;
+	}
+
+}
--- Team.java
+++ Team.java
@@ -1,1 +1,83 @@
-//här är den här classen
+
+public class Team {
+	private String name;		// A simple string with the name of the team
+	private int pnp;			// A simple number of players not playing
+	private int[] games;		// Array with MatchIds
+	private int[] hstatistic; 	// H: 40-10-50 Dashes are added in the graphical part
+	private int[] astatistic; 	// A: 30-10-60
+	private boolean cl;			// True is for champions league, false is for not champions league
+	
+	/**
+	 * The constructor needs a a name for the team, 
+	 * one int with players not playing,
+	 * Int array for the five latest games MID's, Can be null?
+	 * Int arrays for home statistics and away statistics, Can be null?
+	 * And a boolean for champions leauge,
+	 * You can not have a team without all of these.
+	 */
+	Team(String n, int p, int[] g, int[] hs, int[] as, boolean c){ 
+		this.setName(n);
+		this.setPnp(p);
+		this.setGames(g);
+		this.setHstatistic(hs); 
+		this.setAstatistic(as); 
+		this.setCl(c);
+	}
+	/**
+	 * Constructor for inserting into db where you don't need the latest games and statistics
+	 */
+	Team(String n, int p, boolean c){
+		this.setName(n);
+		this.setPnp(p);
+		this.setCl(c);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPnp() {
+		return pnp;
+	}
+
+	public void setPnp(int pnp) {
+		this.pnp = pnp;
+	}
+
+	public int[] getGames() {
+		return games;
+	}
+
+	public void setGames(int[] g) {
+		this.games = g;
+	}
+
+	public boolean isCl() {
+		return cl;
+	}
+
+	public void setCl(boolean cl) {
+		this.cl = cl;
+	}
+
+	public int[] getHstatistic() {
+		return hstatistic;
+	}
+
+	public void setHstatistic(int[] hstatistic) {
+		this.hstatistic = hstatistic;
+	}
+
+	public int[] getAstatistic() {
+		return astatistic;
+	}
+
+	public void setAstatistic(int[] astatistic) {
+		this.astatistic = astatistic;
+	}
+	
+}
